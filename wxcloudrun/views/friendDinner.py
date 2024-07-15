db3d9b6b2a739c82b502a325588375bf424a0cba
fridayli <lidongyong22@gmail.com>
2024-07-15 22:38:37 +0800

    fix bug

--- wxcloudrun/views/friendDinner.py
+++ wxcloudrun/views/friendDinner.py
@@ -16,7 +16,7 @@ class FriendDinnerView(View):
         return super(FriendDinnerView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, openId, *args, **kwargs):
-        dinners = Dinners.objects.filter(user_openId__ne=openId).order_by('-createdAt').all()
+        dinners = Dinners.objects.exclude(user_openId=openId).order_by('-createdAt').all()
         result = [dinner.to_json() for dinner in dinners]
         return JsonResponse(data={'data': result, 'code': 0})
 
