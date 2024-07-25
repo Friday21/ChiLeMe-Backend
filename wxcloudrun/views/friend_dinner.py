e9dd9dc45af492c051ec544d592423e6981ed90d
fridayli <lidongyong22@gmail.com>
2024-07-25 22:15:05 +0800

    fix bug

--- wxcloudrun/views/friend_dinner.py
+++ wxcloudrun/views/friend_dinner.py
@@ -23,7 +23,7 @@ class FriendDinnerView(View):
         friend_dict = dict()
         for friend in friends:
             friend_dict[friend['open_id']] = friend
-        open_ids = [friend['open_id'] for friend in friends]
+        open_ids = [friend['open_id'] for friend in friends if friend['open_id'] != openId]
         dinners = Dinners.objects.filter(user_openId__in=open_ids).order_by('-createdAt')[:1000]
         result = []
         for dinner in dinners:
