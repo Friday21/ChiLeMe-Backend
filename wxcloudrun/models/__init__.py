d71220cd5b9098fc76ac675cd800d731bcd73514
fridayli <fridayli@fridaylis-MacBook-Pro.local>
2026-04-18 13:40:29 +0800

    debug

--- wxcloudrun/models/__init__.py
+++ wxcloudrun/models/__init__.py
@@ -3,5 +3,4 @@ from .user import *
 from .dinner import *
 from .user_note import *
 from .finance import *
-from .chileme import *
-from .time_tracker import TimeRecord, TimeSite
+from .time_tracker import TimeItem, TimeInsight
--- wxcloudrun/views/__init__.py
+++ wxcloudrun/views/__init__.py
@@ -8,7 +8,4 @@ from .friend import *
 from .user_note import *
 from .user_note_history import *
 from .user_note_report import *
-from .chileme_meal import MealAnalyzeView, MealListView, MealDetailView
-from .chileme_pk import PKListView, PKAcceptView, PKDeclineView, PKDetailView, PKSettleView
-from .chileme_profile import ChiLeMeProfileView
 from .time_tracker import TimeUploadView, TimeOverviewView, TimeWeekView, TimeSitesView
