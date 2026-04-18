6d9271486cc8a2a3a1bff381499160936a709378
fridayli <fridayli@fridaylis-MacBook-Pro.local>
2026-04-18 13:46:13 +0800

    update

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -6,7 +6,7 @@ CUR_PATH = os.path.dirname(os.path.realpath(__file__))
 LOG_PATH = os.path.join(os.path.dirname(CUR_PATH), 'logs') # LOG_PATH是存放日志的路径
 if not os.path.exists(LOG_PATH): os.mkdir(LOG_PATH)  # 如果不存在这个logs文件夹，就自动创建一个
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'. 2
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
