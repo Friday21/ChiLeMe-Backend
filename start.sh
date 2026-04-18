0a9ae30837fd2e350c031eebd472c0684d798245
fridayli <fridayli@fridaylis-MacBook-Pro.local>
2026-04-18 13:30:17 +0800

    debug

--- start.sh
+++ start.sh
@@ -14,5 +14,5 @@ service cron start
 # Run the update command once on startup (optional, but good for testing)
 # python3 manage.py update_stock_prices &
 
-# Start Gunicorn
-/root/.local/bin/gunicorn --bind 0.0.0.0:80 --workers 4 wxcloudrun.wsgi:application
+# Start Django development server
+python manage.py runserver 0.0.0.0:80
